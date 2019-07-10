c9edf73c0ec609a5da177e419479749bcd280138
Eliott <eliotts@protonmail.com>
2019-07-10 09:00:52 +0200

    a

--- main.lua
+++ main.lua
@@ -1,15 +1,11 @@
 require("https://raw.githubusercontent.com/EliottmacR/framework_collection/master/framework.lua")
 
 first_time_launch = true
+game_name = "fishing_game"
 
 function _init()
-  print("here")
+  local game_id = get_id_from_name(game_name)
+  launch_game(game_id)
 end
 
-function _update()  
-  
-end
 
-function _draw()
-
-end
