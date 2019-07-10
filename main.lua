4250ad97c6f783322a37d90f927367d19d540803
Eliott <eliotts@protonmail.com>
2019-07-10 09:45:12 +0200

    a

--- main.lua
+++ main.lua
@@ -5,8 +5,8 @@ first_time_launch = true
 game_name = "fishing_game"
 
 function _init()
-  local game_id = get_id_from_name(game_name)
-  launch_game(game_id)
+  -- local game_id = get_id_from_name(game_name)
+  -- launch_game(game_id)
   
   -- if game_list._games then log("yes")  log(game_list._games[1].name) end  
   -- log(games[1].name)
