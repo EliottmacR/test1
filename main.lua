3a0e39cedd299596bc2d6aab2e635bcb2775e091
Eliott <eliotts@protonmail.com>
2019-07-10 09:55:02 +0200

    a

--- main.lua
+++ main.lua
@@ -2,9 +2,11 @@ require("https://raw.githubusercontent.com/EliottmacR/framework_collection/maste
 
 first_time_launch = true
 game_name = "fishing_game"
+game_list = nil
 
 function _init()
-  game_list.get_path_from_id()
+  -- game_list.get_path_from_id()
+  get_path_from_id()
   -- local game_id = get_id_from_name(game_name)
   -- launch_game(game_id)
   
