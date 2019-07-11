2a24e0398a8815e7693c561028cd889e00744ee1
Eliott <eliotts@protonmail.com>
2019-07-11 21:54:10 +0200

    a

--- main.lua
+++ main.lua
@@ -1,11 +1,14 @@
 local link = "https://raw.githubusercontent.com/TRASEVOL-DOG/Collection/master/framework/game_list.lua"
-require(link)
 
 if CASTLE_PREFETCH then
   CASTLE_PREFETCH({
     link
     })
 end
+
+require(link)
+
+if load_game then print("second") end
 
 function love.update()
   if load_game and not loaded then load_game("fishing_game") loaded = true end
